e9554b4165af6df45378194c6f0ef62bc3622cf1
Magyarosi Roland <rolandmagyarosi@gmail.com>
2021-11-07 15:41:53 +0200

    income cards

--- functions.js
+++ functions.js
@@ -25,35 +25,59 @@ function calculateBudet() {
 
 }
 
-function func() {
-    alert("Click");
-
-}
 
 let contents = [];
-// example {id:1592304983049, title: 'Deadpool', year: 2015}
-const addContent = (ev) => {
-    ev.preventDefault();  //to stop the form submitting
+function addContent() {
     let content = {
         id: Date.now(),
         description: document.getElementById('description').value,
         price: document.getElementById('value').value
     }
     contents.push(content);
-    document.getElementById('inputs').reset(); // to clear the form for the next entries
-    //document.querySelector('form').reset();
+    document.getElementById('inputs').reset();
     let description = content.description;
     let price = content.price;
-    //for display purposes only
     console.warn('added', { contents });
     let pre = document.querySelector('#descText');
     let pre2 = document.querySelector('#priceText');
-    pre.textContent = JSON.stringify(description, '\t', 2);
-    pre2.textContent = JSON.stringify(price, '\t', 2);
+    //pre.textContent = JSON.stringify(description, '\t', 2);
+    //pre2.textContent = JSON.stringify(price, '\t', 2);
 
-    //saving to localStorage
     localStorage.setItem('MyContentList', JSON.stringify(contents));
+
+    var div = document.createElement('div');
+
+    //var attr = document.createAttribute("priceText");
+    var h3 = document.createElement("h3"); //create the paragraph tag
+    var p = document.createElement("p"); //create the paragraph tag
+    h3.id += 'descText'; // give it a class by adding to the list
+    p.id += 'priceText'; // give it a class by adding to the list
+    for (let i = 0; i < contents.length; i++) {
+        h3.innerHTML = contents[i].description;
+        p.innerHTML = contents[i].price;
+    }
+    div.appendChild(h3);
+    div.appendChild(p);
+    document.body.appendChild(div);
+    var h3 = document.createElement('h3');
+    var p = document.createElement('p');
+
+    div.className = 'cardforincome';
+    document.getElementsByClassName('content')[0].appendChild(div);
+
 }
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('addButton').addEventListener('click', addContent);
-});
+
+
+//document.addEventListener('DOMContentLoaded', () => {
+
+//document.getElementById('addButton').addEventListener('click', addContent);
+//});
+
+
+
+function loadPage() {
+    updateTime();
+    calculateBudet();
+}
+
+window.onload = loadPage;
